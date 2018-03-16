6edc9b5c8e539e15618ccdcf6929b39a3e0f8080
Unknown <tomivoook23@gmail.com>
2018-03-16 14:07:50 +0100

    Basic files

---
+++ Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PES
+{
+    class Program
+    {
+        bool CLOSE = false;
+        ConsoleKey[] AvailableKeys =
+        {
+            ConsoleKey.A,
+            ConsoleKey.B,
+            ConsoleKey.C,
+            ConsoleKey.D,
+            ConsoleKey.E,
+            ConsoleKey.F,
+            ConsoleKey.G,
+            ConsoleKey.H,
+            ConsoleKey.I,
+            ConsoleKey.J,
+            ConsoleKey.K,
+            ConsoleKey.L,
+            ConsoleKey.M,
+            ConsoleKey.N,
+            ConsoleKey.O,
+            ConsoleKey.P,
+            ConsoleKey.Q,
+            ConsoleKey.R,
+            ConsoleKey.S,
+            ConsoleKey.T,
+            ConsoleKey.U,
+            ConsoleKey.V,
+            ConsoleKey.W,
+            ConsoleKey.X,
+            ConsoleKey.Y,
+            ConsoleKey.Z,
+            ConsoleKey.D0,
+            ConsoleKey.D1,
+            ConsoleKey.D2,
+            ConsoleKey.D3,
+            ConsoleKey.D4,
+            ConsoleKey.D5,
+            ConsoleKey.D6,
+            ConsoleKey.D7,
+            ConsoleKey.D8,
+            ConsoleKey.D9,
+            ConsoleKey.NumPad0,
+            ConsoleKey.NumPad1,
+            ConsoleKey.NumPad2,
+            ConsoleKey.NumPad3,
+            ConsoleKey.NumPad4,
+            ConsoleKey.NumPad5,
+            ConsoleKey.NumPad6,
+            ConsoleKey.NumPad7,
+            ConsoleKey.NumPad8,
+            ConsoleKey.NumPad9,
+            ConsoleKey.OemComma,
+            ConsoleKey.OemPeriod,
+            ConsoleKey.OemMinus,
+            ConsoleKey.OemPlus,
+            ConsoleKey.Add,
+            ConsoleKey.Subtract,
+            ConsoleKey.Spacebar
+        };
+        peScript script;
+
+        static void Main(string[] args)
+        => new Program().Game();
+        
+        private void Game()
+        {
+            GameInitializing();
+            GameLoop();
+        }
+
+        private void GameInitializing()
+        {
+            script = new peScript();
+            script.InitPes("");
+
+            Task.Run(() =>
+            {
+                GameInput();
+            });
+        }
+
+        private void GameLoop()
+        {
+            while(!CLOSE)
+            {
+                Console.WriteLine("text");
+                Thread.Sleep(10000);
+            }
+        }
+
+        private void GameInput()
+        {
+            while (true)
+            {
+                string result = "";
+                var buffer = new List<char>();
+                int cursor_pos = 0;
+                bool enter_pressed = false;
+                do
+                {
+                    result = "";
+                    foreach (var c in buffer)
+                        result += c;
+
+                    int x = Console.CursorLeft;
+                    int y = Console.CursorTop;
+                    Console.CursorTop = Console.WindowTop + Console.WindowHeight - 1;
+                    Console.CursorLeft = 0;
+                    for (int i = 0; i < Console.WindowWidth-1; i++)
+                        Console.Write(' ');
+                    Console.CursorLeft = 0;
+                    Console.CursorTop = Console.WindowTop + Console.WindowHeight - 1;
+                    Console.Write(result);
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.CursorLeft = cursor_pos;
+                    Console.Write(" ");
+                    Console.ResetColor();
+                    // Restore previous position
+                    Console.SetCursorPosition(x, y);
+
+                    var k = Console.ReadKey(true);
+                    if (AvailableKeys.Contains(k.Key))
+                    {
+                        buffer.Insert(cursor_pos, k.KeyChar);
+                        cursor_pos++;
+                    }
+
+                    if (k.Key == ConsoleKey.Enter)
+                    {
+                        enter_pressed = true;
+                    }
+
+                    if (k.Key == ConsoleKey.Backspace && buffer.Count > 0)
+                    {
+                        buffer.RemoveAt(buffer.Count - 1);
+                        cursor_pos--;
+                    }
+
+                    if (k.Key == ConsoleKey.LeftArrow && cursor_pos > 0)
+                        cursor_pos--;
+
+                    if (k.Key == ConsoleKey.RightArrow && cursor_pos < buffer.Count)
+                        cursor_pos++;
+
+                } while (!enter_pressed);
+                cursor_pos = 0;
+                ProcessInput(result);
+            }
+        }
+
+        private void ProcessInput(string buffer)
+        {
+            Console.WriteLine(buffer);
+            buffer = buffer.ToLower();
+            if (buffer.Length > 6 && buffer.Substring(0, 4) == "run.")
+                Console.WriteLine(script.Run(buffer.Substring(4)));
+        }
+    }
+}
---
+++ peScript.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+
+namespace PES
+{
+    class peScript
+    {
+        private List<string[]> MODULES;
+        private List<string> Sarg;
+        private List<float> Farg;
+
+        public void InitPes(string path = "")
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                path = AppDomain.CurrentDomain.BaseDirectory;
+
+            MODULES = new List<string[]>();
+            Console.WriteLine("Searching for modules..");
+            foreach (var file in Directory.GetFiles(path))
+            {
+                string fff = file.Replace(path, "");
+                if (fff.Substring(fff.Length - 4) == ".pem" && fff.Substring(0, 7) == "module_")
+                {
+                    string module_name = fff.Split('_')[1];
+                    string module_version = fff.Split('_')[2];
+                    Console.WriteLine(module_name + "v" + module_version + " loaded.");
+
+                    MODULES.Add(new string[] { path + fff, module_name, module_version });
+                }
+            }
+            Console.WriteLine(MODULES.Count + " modules found.");
+            if (Directory.Exists("tmp"))
+                Directory.Delete("tmp", true);
+            Directory.CreateDirectory("tmp");
+        }
+
+        public string Run(string command)
+        {
+            command = command.ToLower();
+            if (!command.Contains('+'))
+                return "ERR:0143:Argument not found.";
+
+            string[] module_array = new string[] { };
+            string module = command.Split('+')[0];
+
+            string[] args = command.Substring(module.Length).Split('+');
+
+            Sarg = new List<string>();
+            Farg = new List<float>();
+
+            foreach (var arg in args)
+            {
+                if (float.TryParse(arg, out float test))
+                {
+                    Farg.Add(test);
+                }
+                else
+                {
+                    Sarg.Add(arg);
+                }
+            }
+            
+            Sarg.RemoveAt(0);
+
+            bool found = false;
+            for (int i = 0; i < MODULES.Count; i++)
+            {
+                if (MODULES[i][1] == module)
+                {
+                    module_array = MODULES[i];
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                return "ERR:0162:Module not found.";
+
+            if (Directory.Exists(@"tmp\" + module_array[1]))
+                Directory.Delete(@"tmp\" + module_array[1], true);
+            Directory.CreateDirectory(@"tmp\" + module_array[1]);
+            ZipFile.ExtractToDirectory(module_array[0], @"tmp\" + module_array[1]);
+
+            string[] Lines = File.ReadAllLines(@"tmp\" + module_array[1] + @"\code.pes");
+
+            bool Skip = false;
+            foreach (var lineRaw in Lines)
+            {
+                if (string.IsNullOrWhiteSpace(lineRaw) || string.IsNullOrEmpty(lineRaw))
+                    continue;
+                string line = lineRaw;
+                while (true)
+                {
+                    if (line[0] == ' ' || line[0] == '	')
+                    {
+                        line = line.Remove(0, 1);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (Skip)
+                {
+                    if(line.Split(' ')[0].Substring(0, 3) == "END")
+                    {
+                        Skip = false;
+                    }
+                    continue;
+                }
+                Skip = !Process(line);
+            }
+
+            return "";
+        }
+
+        private bool Process(string line)
+        {
+            string command = line.Split(' ')[0];
+            switch (command.ToLower())
+            {
+                case "+":
+                    return true;
+                case "print":
+                    Console.WriteLine(line.Split('"')[1]);
+                    return true;
+                case "ifs":
+                    return IFS(line);
+                case "iff":
+                    return IFF(line);
+            }
+            return false;
+        }
+
+        private bool IFS(string line)
+        {
+            string a1 = line.Split(' ')[1];
+            string sep = line.Split(' ')[2];
+            string a2 = line.Split(' ')[3];
+
+            if (a1[0] == '"')
+                a1 = a1.Split('"')[1];
+            if (a2[0] == '"')
+                a2 = a2.Split('"')[1];
+
+
+            if (a1.Substring(0, 4) == "sarg")
+            {
+                int index1 = a1.Length - 1;
+                int index2 = Convert.ToInt32(a1.Substring(index1));
+                a1 = Sarg[index2];
+            }
+            if (a2.Substring(0, 4) == "sarg")
+                a2 = Sarg[Convert.ToInt32(a2[a1.Length - 1])];
+
+            bool result = a1 == a2;
+            if(sep == "=")
+                return result;
+            if (sep == "!=" || sep == "=!")
+                return !result;
+            return false;
+        }
+
+        private bool IFF(string line)
+        {
+            string tmpa1 = line.Split(' ')[1];
+            string sep = line.Split(' ')[2];
+            string tmpa2 = line.Split(' ')[3];
+
+            float a1;
+            float a2;
+
+            float.TryParse(tmpa1, out a1);
+            float.TryParse(tmpa2, out a2);
+
+            if (tmpa1.Substring(0, 4) == "farg")
+            {
+                int index1 = tmpa1.Length - 1;
+                int index2 = Convert.ToInt32(tmpa1.Substring(index1));
+                a1 = Farg[index2];
+            }
+            if (tmpa2.Substring(0, 4) == "farg")
+            {
+                int index1 = tmpa2.Length - 1;
+                int index2 = Convert.ToInt32(tmpa2.Substring(index1));
+                a2 = Farg[index2];
+            }
+            
+            switch (sep)
+            {
+                case "<":
+                    return a1 < a2;
+                case ">":
+                    return a1 > a2;
+                case "=":
+                    return a1 == a2;
+                case "<=":
+                    return a1 <= a2;
+                case ">=":
+                    return a1 >= a2;
+                case "!=":
+                    return a1 != a2;
+            }
+
+
+            return true;
+        }
+    }
+}
